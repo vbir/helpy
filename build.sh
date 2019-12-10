d48f3a81f32d261a16cf9dde982e85d3d80ced26
vbir <46668802+vbir@users.noreply.github.com>
2019-12-10 11:01:37 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cd ..
+cd /root/
 git clone --no-checkput https://github.com/vbir/helpy.git helpy/helpy.tmp
-rm -rf helpy/.git/
+rm -rf helpy/.git
 mv helpy/helpy.tmp/.git helpy/
 rmdir helpy/helpy.tmp
-cd helpy
+cd /root/helpy/
 git reset --hard HEAD
 docker-compose up -d --no-deps --build
-cd ..
+cd /root/
